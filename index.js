9034cea99e89a3ad37b4cbd2f5fcb6e7b2402642
sidsahu172 <siddharth.sahu172@gmail.com>
2017-10-25 13:10:19 +0530

    First Repository

---
+++ index.js
@@ -0,0 +1,47 @@
+//process.stdout.write("Hello\n")
+//console.log("World")
+
+
+global.a="abc" // for backend
+//window.b=124 //for front end
+b=12
+console.log(a)
+console.log(b)
+//console.log(process)
+
+var abc=23;
+//Static memory
+
+// Dynamic memory allocated in heap
+var z={};
+
+// function
+fun(abc)
+console.log(abc)
+function fun(arg1){
+	arg1=2
+}
+fun(abc)
+console.log(abc)
+
+var li=[{},{},"avcb",null,undefined]
+
+// Datatypes
+//Integer
+var abc=0
+//String
+var c="lol"
+
+//Objects
+var obj={};
+var obj2=new Object();
+
+//Prototype Chainn
+var ch={"xyz":"lop"};
+ch.toString();
+ch.hasOwnProperty
+obj={1:"a",2:'b',3:'c',4:'d'}
+
+
+
+
